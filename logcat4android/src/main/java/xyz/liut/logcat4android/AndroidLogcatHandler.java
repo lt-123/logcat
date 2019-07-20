e71ff0c4c84ad342745697be7ca27fc53c5b803d
liut <lt-123@qq.com>
2019-07-20 18:14:04 +0800

    j frog

--- logcat4android/src/main/java/xyz/liut/logcat4android/AndroidLogcatHandler.java
+++ logcat4android/src/main/java/xyz/liut/logcat4android/AndroidLogcatHandler.java
@@ -1,10 +1,9 @@
-package xyz.liut.logcat.android;
+package xyz.liut.logcat4android;
 
 import android.util.Log;
 import org.jetbrains.annotations.NotNull;
 import xyz.liut.logcat.LogHandler;
 import xyz.liut.logcat.LogLevel;
-import xyz.liut.logcat4android.BuildConfig;
 
 /**
  * android logcat
